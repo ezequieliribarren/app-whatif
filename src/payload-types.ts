2d96f2c574c7924f66b00f23e0733daa4f80c591
borren <ezequielgiribarren@gmail.com>
2025-07-25 17:37:58 -0300

    a

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -250,7 +250,21 @@ export interface TeamMember {
    */
   order?: number | null;
   image: string | Media;
-  detail?: string | null;
+  detail?: {
+    root: {
+      type: string;
+      children: {
+        type: string;
+        version: number;
+        [k: string]: unknown;
+      }[];
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
+      indent: number;
+      version: number;
+    };
+    [k: string]: unknown;
+  } | null;
   updatedAt: string;
   createdAt: string;
 }
